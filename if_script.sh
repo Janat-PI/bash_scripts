045fba2e9dedfdb526ee6667dc775cd7cc1e73c5
Zhanat <janat.200066@gmail.com>
2021-11-12 12:42:22 +0600

    if elif else then

--- if_script.sh
+++ if_script.sh
@@ -1,11 +1,42 @@
 #! /bin/bash
 
-user_name=zhanat
-if grep $user_name /etc/passwd
+# user_name=zhanat
+# if grep $user_name /etc/passwd
+# then
+# echo "пользователь найден в системе!"
+# cd /home/$user_name
+# ls
+# else
+# echo "пользователь не найден в системе!"
+# fi
+
+# number1=5
+# number2=10
+# if [ $number1 -eq $number2 ]
+# then 
+# echo "Значения равны"
+# elif [ $number1 -gt $number2 ]
+# then
+# echo "Значения $number1 больше значения $number2"
+# elif [ $number1 -lt $number2 ]
+# then
+# echo "Значения $number1 меньше значения $number2"
+# else
+# echo "Значения не понятное"
+# fi
+
+str1=test
+str2=t
+
+if [ $str1 > $str2 ]
+then 
+echo "первая строка больше"
+elif [ $str1 < $str2 ]
 then
-echo "пользователь найден в системе!"
-cd /home/$user_name
-ls
+echo "вторая строка больше"
+elif [ $str1 == $str2 ]
+then 
+echo "оба равны"
 else
-echo "пользователь не найден в системе!"
+echo "Непонятное значение"
 fi
